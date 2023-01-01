5b9ffe3cbd54dfee14ff51aa2de61511778d4aa0
Bruno Schaatsbergen <git@bschaatsbergen.com>
2023-01-01 20:24:59 +0100

    Expose container image through a var

--- atlantis.tf
+++ atlantis.tf
@@ -1,13 +1,9 @@
-locals {
-  image_tag = var.pull_latest_prerelease ? "prerelease-latest" : "latest"
-}
-
 module "atlantis" {
   source  = "terraform-google-modules/container-vm/google"
   version = "~> 2.0"
 
   container = {
-    image = "ghcr.io/runatlantis/atlantis:${local.image_tag}"
+    image = var.image
     securityContext = {
       privileged : true
     }
