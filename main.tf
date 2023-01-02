9c4115838137c70613dfa1d08f17c9ffc332a45b
Bruno Schaatsbergen <git@bschaatsbergen.com>
2023-01-03 00:44:06 +0100

    Remove atlantis.tf

--- main.tf
+++ main.tf
@@ -117,6 +117,46 @@ resource "google_compute_health_check" "atlantis" {
   }
 }
 
+module "atlantis" {
+  source  = "terraform-google-modules/container-vm/google"
+  version = "~> 2.0"
+
+  container = {
+    image = var.image
+    securityContext = {
+      privileged : true
+    }
+    tty : true
+    env = [for key, value in var.env_vars : {
+      name  = key
+      value = value
+    }]
+
+    # Declare volumes to be mounted.
+    # This is similar to how docker volumes are declared.
+    volumeMounts = [
+      {
+        mountPath = "/home/atlantis"
+        name      = "atlantis-disk-0"
+        readOnly  = false
+      },
+    ]
+  }
+
+  volumes = [
+    {
+      name = "atlantis-disk-0"
+
+      gcePersistentDisk = {
+        pdName = "atlantis-disk-0"
+        fsType = "ext4"
+      }
+    },
+  ]
+
+  restart_policy = "Always"
+}
+
 resource "google_compute_instance_group_manager" "atlantis" {
   name               = var.name
   base_instance_name = var.name
