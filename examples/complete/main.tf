7df4b65624a2764c83c8ae1bac3f56523856e951
Bruno Schaatsbergen <git@bschaatsbergen.com>
2023-01-05 22:03:35 +0100

    move to locals

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,35 +1,50 @@
-provider "google" {
-  project = "<your-project-id>"
+locals {
+  project_id            = "<your-project-id>"
+  network               = "<your-network>"
+  subnetwork            = "<your-subnetwork>"
+  region                = "<your-region>"
+  zone                  = "<your-zone>"
+  service_account_email = "<your-service-account-email>"
+  domain                = "<example.com>"
+  managed_zone          = "<your-managed-zone>"
+
+  github_repo_allow_list = "github.com/example/*"
+  github_user            = "<your-github-handle>"
+  github_token           = "<your-github-user>"
+  github_webhook_secret  = "<your-github-webhook-secret>"
 }
 
 # Create a service account and attach the required Cloud Logging permissions to it.
 resource "google_service_account" "atlantis" {
   account_id   = "atlantis-sa"
   display_name = "Service Account for Atlantis"
+  project      = local.project_id
 }
 
 resource "google_project_iam_member" "atlantis_log_writer" {
   role    = "roles/logging.logWriter"
   member  = "serviceAccount:${google_service_account.atlantis.email}"
-  project = "<your-project-id>"
+  project = local.project_id
 }
 
 resource "google_project_iam_member" "atlantis_metric_writer" {
   role    = "roles/monitoring.metricWriter"
   member  = "serviceAccount:${google_service_account.atlantis.email}"
-  project = "<your-project-id>"
+  project = local.project_id
 }
 
 resource "google_compute_network" "default" {
   name                    = "example-network"
   auto_create_subnetworks = false
+  project                 = local.project_id
 }
 
 resource "google_compute_subnetwork" "default" {
   name          = "example-subnetwork"
   ip_cidr_range = "10.2.0.0/16"
-  region        = "<your-region>"
+  region        = local.region
   network       = google_compute_network.default.id
+  project       = local.project_id
 }
 
 # Create a router, which we associate the Cloud NAT too
@@ -41,6 +56,7 @@ resource "google_compute_router" "default" {
   bgp {
     asn = 64514
   }
+  project = local.project_id
 }
 
 # Create a NAT for outbound internet traffic
@@ -50,6 +66,7 @@ resource "google_compute_router_nat" "default" {
   region                             = google_compute_router.default.region
   nat_ip_allocate_option             = "AUTO_ONLY"
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+  project                            = local.project_id
 }
 
 module "atlantis" {
@@ -57,33 +74,35 @@ module "atlantis" {
   name       = "atlantis"
   network    = google_compute_network.default.name
   subnetwork = google_compute_subnetwork.default.name
-  region     = "<your-region>"
-  zone       = "<your-zone>"
+  region     = local.region
+  zone       = local.zone
   service_account = {
-    email  = google_service_account.atlantis.email
+    email  = local.service_account_email
     scopes = ["cloud-platform"]
   }
   # Note: environment variables are shown in the Google Cloud UI
   # See the `examples/secured-env-vars` if you want to protect sensitive information
   env_vars = {
-    ATLANTIS_GH_USER           = "<your-github-user>"
-    ATLANTIS_GH_TOKEN          = "<your-github-token>"
-    ATLANTIS_GH_WEBHOOK_SECRET = "<your-github-webhook-secret>"
-    ATLANTIS_REPO_ALLOWLIST    = "github.com/<your-github-handle>/*"
-    ATLANTIS_ATLANTIS_URL      = "https://<your-domain>"
+    ATLANTIS_GH_USER           = local.github_user
+    ATLANTIS_GH_TOKEN          = local.github_token
+    ATLANTIS_GH_WEBHOOK_SECRET = local.github_webhook_secret
+    ATLANTIS_REPO_ALLOWLIST    = local.github_repo_allow_list
+    ATLANTIS_ATLANTIS_URL      = "https://${local.domain}"
     ATLANTIS_REPO_CONFIG_JSON  = jsonencode(yamldecode(file("${path.module}/server-atlantis.yaml")))
   }
-  domain = "<your-domain>"
+  domain  = local.domain
+  project = local.project_id
 }
 
 # As your DNS records might be managed at another registrar's site, we create the DNS record outside of the module.
 # This record is mandatory in order to provision the managed SSL certificate successfully.
 resource "google_dns_record_set" "default" {
-  name         = "<your-domain>."
+  name         = "${local.domain}."
   type         = "A"
   ttl          = 60
-  managed_zone = "<your-managed-zone>"
+  managed_zone = local.managed_zone
   rrdatas = [
     module.atlantis.ip_address
   ]
+  project = local.project_id
 }
--- examples/secured-env-vars/main.tf
+++ examples/secured-env-vars/main.tf
@@ -1,51 +1,67 @@
+locals {
+  project_id            = "<your-project-id>"
+  network               = "<your-network>"
+  subnetwork            = "<your-subnetwork>"
+  region                = "<your-region>"
+  zone                  = "<your-zone>"
+  image                 = "<your-image>"
+  service_account_email = "<your-service-account-email>"
+  domain                = "<example.com>"
+  managed_zone          = "<your-managed-zone>"
+
+  github_repo_allow_list = "github.com/example/*"
+}
+
 # Create a service account and attach the required Cloud Logging permissions to it.
 resource "google_service_account" "atlantis" {
   account_id   = "atlantis"
   display_name = "Service Account for Atlantis"
+  project      = local.project_id
 }
 
 resource "google_project_iam_member" "atlantis_log_writer" {
   role    = "roles/logging.logWriter"
   member  = "serviceAccount:${google_service_account.atlantis.email}"
-  project = "<your-project-id>"
+  project = local.project_id
 }
 
 resource "google_project_iam_member" "atlantis_metric_writer" {
   role    = "roles/monitoring.metricWriter"
   member  = "serviceAccount:${google_service_account.atlantis.email}"
-  project = "<your-project-id>"
-}
-
-# As your DNS records might be managed at another registrar's site, we create the DNS record outside of the module.
-# This record is mandatory in order to provision the managed SSL certificate successfully.
-resource "google_dns_record_set" "default" {
-  name         = "<your-domain>."
-  type         = "A"
-  ttl          = 60
-  managed_zone = "<your-managed-zone>"
-  rrdatas = [
-    module.atlantis.ip_address
-  ]
-  project = var.project_id
+  project = local.project_id
 }
 
 module "atlantis" {
   source     = "bschaatsbergen/atlantis/gce"
   name       = "atlantis"
-  image      = "<your-image>" # Your wrapper Atlantis Docker image
-  network    = "<your-network>"
-  subnetwork = "<your-subnetwork>"
-  region     = "<your-region>"
-  zone       = "<your-zone>"
+  image      = local.image # Your wrapper Atlantis Docker image
+  network    = local.network
+  subnetwork = local.subnetwork
+  region     = local.region
+  zone       = local.zone
   service_account = {
-    email  = "<your-service-account-email>"
+    email  = local.service_account_email
     scopes = ["cloud-platform"]
   }
   # Declare the non-sensitive environment variables here
   # The sensitive environment variables are set in the Dockerfile!
   env_vars = {
-    ATLANTIS_REPO_ALLOWLIST = "github.com/<your-github-handle>/*"
-    ATLANTIS_ATLANTIS_URL   = "https://<your-domain>"
+    ATLANTIS_REPO_ALLOWLIST = local.github_repo_allow_list
+    ATLANTIS_ATLANTIS_URL   = "https://${local.domain}"
   }
-  domain = "<your-domain>"
+  domain  = local.domain
+  project = local.project_id
+}
+
+# As your DNS records might be managed at another registrar's site, we create the DNS record outside of the module.
+# This record is mandatory in order to provision the managed SSL certificate successfully.
+resource "google_dns_record_set" "default" {
+  name         = "${local.domain}."
+  type         = "A"
+  ttl          = 60
+  managed_zone = local.managed_zone
+  rrdatas = [
+    module.atlantis.ip_address
+  ]
+  project = local.project_id
 }
