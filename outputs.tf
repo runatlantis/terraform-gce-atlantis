abd16921df595ee8fac6ae3c796d87b8635de5e8
Bruno Schaatsbergen <git@bschaatsbergen.com>
2023-01-08 00:04:40 +0100

    Add cos image ID and managed SSL outputs

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,18 @@ output "ip_address" {
   value       = google_compute_global_address.default.address
   description = "The IPv4 address of the load balancer"
 }
+
+output "cos_image_id" {
+  value       = data.google_compute_image.cos.image_id
+  description = "The unique identifier for the Container-Optimized OS image used to create the Compute Engine instance."
+}
+
+output "managed_ssl_certificate_certificate_id" {
+  value       = google_compute_managed_ssl_certificate.default.certificate_id
+  description = "The unique identifier for the Google Managed SSL certificate"
+}
+
+output "managed_ssl_certificate_expire_time" {
+  value       = google_compute_managed_ssl_certificate.default.expire_time
+  description = "Expire time of the Google Managed SSL certificate"
+}
