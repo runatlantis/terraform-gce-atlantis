877024b367a67286fcbf5a6090a47202495ed235
cblkwell <cblackwell@redoxengine.com>
2024-11-04 08:58:14 -0600

    Bumping version requirement due to iap enabled setting
    Signed-off-by: cblkwell <cblackwell@redoxengine.com>
    Signed-off-by: dimisjim <dimitrios.moraitidis@radicant.com>

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">=4.79.0"
+      version = ">=6.9.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
