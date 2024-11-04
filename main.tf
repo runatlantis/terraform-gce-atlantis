7c1ad5953a049bec23bc663aa5570f1b4724bad2
cblkwell <cblackwell@redoxengine.com>
2024-11-04 08:54:10 -0600

    Adding enabled = true for iap backend
    Signed-off-by: cblkwell <cblackwell@redoxengine.com>

--- main.tf
+++ main.tf
@@ -345,6 +345,7 @@ resource "google_compute_backend_service" "iap" {
   }
 
   iap {
+    enabled              = true
     oauth2_client_id     = var.iap.oauth2_client_id
     oauth2_client_secret = var.iap.oauth2_client_secret
   }
